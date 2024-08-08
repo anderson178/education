0f9ca08575519c9799410c5a63078b41f0cab8dd
mironenko <mironenko@protei.ru>
2024-08-08 15:07:32 +0300

    Threads.simple - Общие сведения. Разбор самых простых и базовых вещей в потоках

---
+++ multithreading/src/main/java/ru/education/threads/stepik/Simple.java
@@ -0,0 +1,42 @@
+package ru.education.threads.stepik;
+
+public class Simple {
+    public static void main(String[] args) throws InterruptedException {
+        Runnable task1 =()-> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i);
+            }
+        };
+
+        Runnable task2 =()-> {
+            for (int i = 10; i < 20; i++) {
+                System.out.println(i);
+            }
+        };
+
+        Thread thread1 = new Thread(task1);
+        Thread thread2 = new Thread(task2);
+        thread1.start();
+        thread2.start();
+
+
+        main2(args);
+    }
+
+    public static void print() {
+        int i = 5;
+        System.out.println(i);
+        i += 10;
+        System.out.println(i);
+    }
+
+    public static void main2(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(Simple::print);
+        Thread t2 = new Thread(Simple::print);
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+    }
+}
